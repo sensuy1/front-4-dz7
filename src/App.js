e3c7be27a3fc4cbcda352ed8b1a5c98b4f0c366e
sensuy1 <sultanbejsenbekov@gmail.com>
2024-02-03 13:11:48 +0600

    dz7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo, useCallback } from 'react';
 
-function App() {
+const MyComponent = () => {
+  const [value, setValue] = useState('')
+
+  const reversedValue = useMemo(() => {
+    console.log('Calculating reversed value...')
+    return value.split('').reverse().join('')
+  }, [value])
+
+  const handleChange = useCallback((event) => {
+    setValue(event.target.value)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={value} onChange={handleChange} />
+      <p>Reversed Value: {reversedValue}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default MyComponent
+
+
